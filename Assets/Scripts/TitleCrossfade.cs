e154d7422b57dd57d617dc186f2124f8b21cd03a
Fabien Chavonet <fchavonet@mailo.com>
2024-01-28 09:23:05 +0100

    Create Walls for the Hub Scene.

--- Assets/Scripts/TitleCrossfade.cs
+++ Assets/Scripts/TitleCrossfade.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class Crossfade : MonoBehaviour
+public class TitleCrossfade : MonoBehaviour
 {
     [Space(10)]
     public Animator animator;
