28a1b22b24abbf525cbd6722b407e74ae5018585
Fabien Chavonet <fchavonet@mailo.com>
2024-01-28 09:23:05 +0100

    Create Walls for the Hub Scene.

---
+++ Assets/Scripts/HubCrossfade.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubCrossfade : MonoBehaviour
+{
+    [Space(10)]
+    public Animator animator;
+    [Space(10)]
+    public float transitionTime = 1f;
+
+    public void NextLevelCrossfade()
+    {
+        StartCoroutine(loadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator loadLevel(int levelIndex)
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
