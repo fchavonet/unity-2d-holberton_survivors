17d887a2001f0bf91266446cada255091b279120
fchavonet <fchavonet@mailo.com>
2024-01-31 11:01:03 +0100

    Create player health bar.

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,49 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+
+    [Space(10)]
+    public Animator animator;
+    public Slider healthSlider;
+    [Space(10)]
+    public float currentHealth;
+    public float maxHealth = 100f;
+
+    void Awake()
+    {
+        instance = this;
+    } 
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnHit() {
+        TakeDamage(10f);
+    }
+
+    public void TakeDamage(float damageToTake)
+    {
+        currentHealth -= damageToTake;
+
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+        healthSlider.value = currentHealth;
+    }
+}
