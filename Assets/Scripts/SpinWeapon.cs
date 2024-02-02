458560dff518033f1b27eb9d0a4cc267735b6765
fchavonet <fchavonet@mailo.com>
2024-02-02 15:43:48 +0100

    Make shurikens hurt enemies.

--- Assets/Scripts/SpinWeapon.cs
+++ Assets/Scripts/SpinWeapon.cs
@@ -2,12 +2,26 @@ using UnityEngine;
 
 public class SpinWeapon : MonoBehaviour
 {
+    [Space(10)]
     public Transform holder;
+    public Transform shurikenToSpawn;
+
     [Space(10)]
     public float rotateSpeed = 180f;
+    public float timeBetweenSpawn = 5f;
+
+    private float spawnCounter;
 
     void Update()
     {
         holder.rotation = Quaternion.Euler(0f, 0f, holder.rotation.eulerAngles.z + (rotateSpeed * Time.deltaTime));
+
+        spawnCounter -= Time.deltaTime;
+        if (spawnCounter <= 0)
+        {
+            spawnCounter = timeBetweenSpawn;
+
+            Instantiate(shurikenToSpawn, shurikenToSpawn.position, shurikenToSpawn.rotation, holder).gameObject.SetActive(true);
+        }
     }
 }
