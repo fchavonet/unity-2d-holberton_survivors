21b5f23dea24acccf3dac2cd725b1684f716e16f
fchavonet <fchavonet@mailo.com>
2024-02-04 18:52:49 +0100

    Create InfiniteGrid behavior script.

---
+++ Assets/Scripts/InfiniteGrid.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class InfiniteGrid : MonoBehaviour
+{
+    [Space(10)]
+    public Transform target;
+    [Space(10)]
+    public float snap = 2f;
+
+    void Update()
+    {
+        Vector2 position = new Vector2(Mathf.Round(target.position.x / snap) * snap, Mathf.Round(target.position.y / snap) * snap);
+        transform.position = position;
+    }
+}
