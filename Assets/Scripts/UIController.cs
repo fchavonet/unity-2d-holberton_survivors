5fef5cd1795f66090e952f87a119f56cb9833b10
fchavonet <fchavonet@mailo.com>
2024-02-14 22:44:16 +0100

    Create pause basic menu.

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
@@ -14,9 +15,26 @@ public class UIController : MonoBehaviour
 
     public GameObject levelUpPanel;
 
+    public string mainMenuName;
+
+    public GameObject pauseScreen;
+
     private void Awake()
     {
         instance = this;
+    }
+
+    /*void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseUnpause();
+        }
+    }*/
+
+    public void OnPressPause()
+    {
+        PauseUnpause();
     }
 
     public void UpdateExperience(int currentExperience, int levelExperience, int currentLevel)
@@ -31,5 +49,39 @@ public class UIController : MonoBehaviour
     {
         levelUpPanel.SetActive(false);
         Time.timeScale = 1f;
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuName);
+        Time.timeScale = 1f;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+
+    public void PauseUnpause()
+    {
+        if (pauseScreen.activeSelf == false)
+        {
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseScreen.SetActive(false);
+            if (levelUpPanel.activeSelf == false)
+            {
+                Time.timeScale = 1f;
+            }
+        }
     }
 }
