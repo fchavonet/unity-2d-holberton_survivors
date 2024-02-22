d37117de76697f9d4bce833610fe87000532a850
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-02-22 18:05:19 +0100

    Add fade out music in Title Screen.

--- Assets/Scripts/TitleCrossfade.cs
+++ Assets/Scripts/TitleCrossfade.cs
@@ -10,6 +10,10 @@ public class TitleCrossfade : MonoBehaviour
     [Space(10)]
     public float transitionTime = 1f;
 
+    [Space(10)]
+    public AudioSource audioSource;
+    public float musicFadeOutTime = 1f;
+
     public void OnPressStart()
     {
         NextLevelCrossfade();
@@ -18,14 +22,34 @@ public class TitleCrossfade : MonoBehaviour
     public void NextLevelCrossfade()
     {
         StartCoroutine(loadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        StartCoroutine(FadeOutMusic());
     }
 
     IEnumerator loadLevel(int levelIndex)
     {
+
         animator.SetTrigger("Start");
 
         yield return new WaitForSeconds(transitionTime);
+        yield return new WaitForSeconds(musicFadeOutTime);
 
         SceneManager.LoadScene(levelIndex);
+    }
+
+    IEnumerator FadeOutMusic()
+    {
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        float startVolume = audioSource.volume;
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / musicFadeOutTime;
+            yield return null;
+        }
+
+        audioSource.Stop();
     }
 }
