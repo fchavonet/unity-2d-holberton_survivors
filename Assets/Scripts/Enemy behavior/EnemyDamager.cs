655cbc544a5e8aadfb60103e96f7b300774568df
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-03-10 00:49:37 +0100

    Clean and comment Code. (peer coding)

--- Assets/Scripts/Enemy behavior/EnemyDamager.cs
+++ Assets/Scripts/Enemy behavior/EnemyDamager.cs
@@ -4,42 +4,58 @@ using UnityEngine;
 public class EnemyDamager : MonoBehaviour
 {
     [Space(10)]
+    // Specifies the damage dealt to enemies.
     public float damageAmount = 5f;
+    // Duration before the damage object is automatically destroyed.
     public float lifeTime = 3f;
+    // The speed at which the damage object grows to its target size.
     public float growSpeed = 5f;
 
     [Space(10)]
     public bool destroyParent;
+    // If true, enemies will be knocked back when damaged.
     public bool shouldKnockBack;
+    // If true, the damage source is destroyed upon first impact with an enemy.
     public bool destroyOnImpact;
 
     [Space(10)]
+    // Enables or disables damage over time functionality.
     public bool damageOverTime;
+    // Time interval between successive damage applications in DoT mode.
     public float timeBetweenDamage;
 
+    // Tracks time between DoT applications.
     private float damageCounter;
+    // List of enemies currently within damage range for DoT.
     private List<EnemyController> enemiesInRange = new List<EnemyController>();
+    // List of bosses currently within damage range for DoT.
     private List<BossController> bossInRange = new List<BossController>();
+    // The intended final size of the damage source object.
     private Vector3 targetSize;
 
     void Start()
     {
+        // Initialize the target size and set the initial scale to zero
         targetSize = transform.localScale;
         transform.localScale = Vector3.zero;
     }
 
     void Update()
     {
+        // Grow the object to its target size over time
         transform.localScale = Vector3.MoveTowards(transform.localScale, targetSize, growSpeed * Time.deltaTime);
 
+        // Decrease the lifetime of the object and check for expiration
         lifeTime -= Time.deltaTime;
-
         if (lifeTime <= 0)
         {
+            // Begin shrinking the object down to removal size
             targetSize = Vector3.zero;
-
+            
+            // Check if the object has completely shrunk
             if (transform.localScale.x == 0f)
             {
+                // Destroy this object and optionally its parent
                 Destroy(gameObject);
 
                 if (destroyParent)
@@ -49,8 +65,10 @@ public class EnemyDamager : MonoBehaviour
             }
         }
 
+        // Applies damage over time if enabled.
         if (damageOverTime == true)
         {
+            // Resets the damage timer and applies damage to all enemies in range.
             damageCounter -= Time.deltaTime;
 
             if (damageCounter <= 0)
@@ -65,11 +83,13 @@ public class EnemyDamager : MonoBehaviour
                     }
                     else
                     {
+                        // Removes any null references from the list.
                         enemiesInRange.RemoveAt(i);
                         i--;
                     }
                 }
 
+                // Applies damage to all bosses in range in a similar manner.
                 for (int b = 0; b < bossInRange.Count; b++)
                 {
                     if (bossInRange[b] != null)
@@ -88,26 +108,32 @@ public class EnemyDamager : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Direct damage or addition to the DOT list upon collision.
         if (damageOverTime == false)
         {
+            // Instant damage on collision.
             if (collision.tag == "Enemy" || collision.tag == "Boss")
             {
                 if(collision.CompareTag("Boss"))
                 {
+                    // Applies damage to the boss.
                     collision.GetComponent<BossController>().TakeDamage(damageAmount, shouldKnockBack);
                 }
                 else
                 {
-                collision.GetComponent<EnemyController>().TakeDamage(damageAmount, shouldKnockBack);
+                    // Applies damage to an enemy.
+                    collision.GetComponent<EnemyController>().TakeDamage(damageAmount, shouldKnockBack);
                 }
                 if(destroyOnImpact)
                 {
+                    // Destroys the damage object on impact.
                     Destroy(gameObject);
                 }
             }
         }
         else
         {
+            // Adds the collided enemy or boss to the appropriate list for DOT.
             if (collision.tag == "Enemy" || collision.tag == "Boss")
             {
                 if (collision.CompareTag("Boss"))
@@ -124,6 +150,7 @@ public class EnemyDamager : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        // Removes enemies or bosses from the DOT list when they exit the trigger area.
         if (damageOverTime)
         {
             if (collision.CompareTag("Enemy"))
--- Assets/Scripts/Enemy behavior/EnemySpawner.cs
+++ Assets/Scripts/Enemy behavior/EnemySpawner.cs
@@ -3,35 +3,54 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    // Variables for defining spawn area
     [Space(10)]
+    // The minimum point of the spawn area.
     public Transform minSpawn;
+    // The maximum point of the spawn area.
     public Transform maxSpawn;
 
+    // Variables for enemy to spawn
     [Space(10)]
+    // The enemy prefab to spawn.
     public GameObject enemyToSpawn;
 
     [Space(10)]
+    // Time between each enemy spawn.
     public float timeToSpawn;
 
+    // Variables for despawning and tracking
     [Space(10)]
+    // Number of enemies to check for despawning per frame.
     public int checkPerFrame;
+    // List of WaveInfo objects defining different waves of enemy spawns.
     public List<WaveInfo> waves;
 
+    // List to keep track of spawned enemies
     public List<GameObject> spawnedEnemies = new List<GameObject>();
-    
+
+    // Index of the enemy to check for despawning.
     private int enemyToCheck;
+    // Target position for the spawner to follow (usually the player).
     private Transform target;
+    // Distance from the spawner where enemies will despawn.
     private float despawnDistance;
+    // Countdown timer for spawning enemies.
     private float spawnCounter;
+    // Index of the current wave.
     private int currentWave;
+    // Countdown timer for the current wave duration.
     private float waveCounter;
 
     void Start()
     {
+        // Initialization
         spawnCounter = timeToSpawn;
         target = PlayerHealthController.instance.transform;
+        // Calculate the distance for despawning enemies
         despawnDistance = Vector3.Distance(transform.position, maxSpawn.position) + 5f;
         currentWave = -1;
+        // Start the first wave.
         GoToNextWave();
     }
 
@@ -39,26 +58,33 @@ public class EnemySpawner : MonoBehaviour
     {
         if (PlayerHealthController.instance.gameObject.activeSelf)
         {
+            // Ensure the player is active before continuing wave and spawn logic
             if (currentWave < waves.Count)
             {
+                // Countdown for the current wave
                 waveCounter -= Time.deltaTime;
                 if (waveCounter <= 0)
                 {
+                    // Move to the next wave when the current wave duration is over
                     GoToNextWave();
                 }
                 spawnCounter -= Time.deltaTime;
                 if (spawnCounter <= 0)
                 {
+                    // Spawn an enemy when the spawn counter reaches zero
                     spawnCounter = waves[currentWave].timeBetweenSpawns;
 
                     GameObject newEnemy = Instantiate(waves[currentWave].enemyToSpawn, SelectSpawnPoint(), Quaternion.identity);
 
+                    // Add the spawned enemy to the list
                     spawnedEnemies.Add(newEnemy);
                 }
             }
         }
+        // Move the spawner position to the target (player)
         transform.position = target.position;
 
+        // Despawn logic for enemies out of range
         int checkTarget = enemyToCheck + checkPerFrame;
 
         while (enemyToCheck < checkTarget)
@@ -69,36 +95,44 @@ public class EnemySpawner : MonoBehaviour
                 {
                     if (Vector3.Distance(transform.position, spawnedEnemies[enemyToCheck].transform.position) > despawnDistance)
                     {
+                        // Destroy the enemy and remove from the list if it's out of range
                         Destroy(spawnedEnemies[enemyToCheck]);
                         spawnedEnemies.RemoveAt(enemyToCheck);
                         checkTarget--;
                     }
                     else
                     {
+                        // Move to the next enemy to check
                         enemyToCheck++;
                     }
                 }
                 else
                 {
+                    // Remove null enemies from the list
                     spawnedEnemies.RemoveAt(enemyToCheck);
                     checkTarget--;
                 }
             }
             else
             {
+                // Reset the check index if it exceeds the list size
                 enemyToCheck = 0;
                 checkTarget = 0;
             }
         }
     }
 
+    // Selects a random spawn point within the defined area
     public Vector3 SelectSpawnPoint()
     {
         Vector3 spawnPoint = Vector3.zero;
+        // Determine whether to spawn near vertical or horizontal edge
         bool spawVerrticalEdge = Random.Range(0f, 1f) > .5f;
         if (spawVerrticalEdge)
         {
+            // Randomly select y-coordinate within spawn area
             spawnPoint.y = Random.Range(minSpawn.position.y, maxSpawn.position.y);
+            // Randomly choose to spawn at the max or min x-position
             if (Random.Range(0f, 1f) > .5f)
             {
                 spawnPoint.x = maxSpawn.position.x;
@@ -110,7 +144,9 @@ public class EnemySpawner : MonoBehaviour
         }
         else
         {
+            // Randomly select x-coordinate within the spawn area
             spawnPoint.x = Random.Range(minSpawn.position.x, maxSpawn.position.x);
+            // Randomly choose to spawn at the max or min y-position
             if (Random.Range(0f, 1f) > .5f)
             {
                 spawnPoint.y = maxSpawn.position.y;
@@ -123,27 +159,34 @@ public class EnemySpawner : MonoBehaviour
         return spawnPoint;
     }
 
+    // Move to the next wave in the list
     public void GoToNextWave()
     {
         currentWave++;
+        // Ensure the current wave index does not exceed the number of waves
         if (currentWave >= waves.Count)
         {
             currentWave = waves.Count - 1;
         }
+        // Set timers for the next wave
         waveCounter = waves[currentWave].waveLength;
         spawnCounter = waves[currentWave].timeBetweenSpawns;
     }
 
+    // Stop generating enemies
     public void StopEnemyGeneration()
     {
-        enabled = false; // Désactiver ce script pour arrêter la génération d'ennemis
+        enabled = false;
     }
 }
 
 [System.Serializable]
 public class WaveInfo
 {
+    // The enemy prefab to spawn for this wave.
     public GameObject enemyToSpawn;
+    // Duration of this wave.
     public float waveLength = 10f;
+    // Time between each enemy spawn in this wave.
     public float timeBetweenSpawns = 1f;
 }
