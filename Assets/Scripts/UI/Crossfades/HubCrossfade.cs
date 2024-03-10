11e889f03e14674c78db875a91bc8f68e67dc297
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-03-10 17:10:41 +0100

    Clean and comment Code. (peer coding)

--- Assets/Scripts/UI/Crossfades/HubCrossfade.cs
+++ Assets/Scripts/UI/Crossfades/HubCrossfade.cs
@@ -5,56 +5,76 @@ using UnityEngine.SceneManagement;
 public class HubCrossfade : MonoBehaviour
 {
     [Space(10)]
+    // Animator for the crossfade transition
     public Animator crossfadeAnimator;
+    // Animator for the door opening animation
     public Animator doorAnimator;
 
     [Space(10)]
+    // Duration of the crossfade transition
     public float transitionTime = 1f;
 
     [Space(10)]
+    // Audio source for background music
     public AudioSource audioSource;
+    // Time to fade out the music
     public float musicFadeOutTime = 1f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-
+        // Check if the collider is tagged as "Door"
         if (other.CompareTag("Door"))
         {
+            // Start the crossfade animation and load the next level
             NextLevelCrossfade();
+            // Trigger the door opening animation
             doorAnimator.SetTrigger("Open");
+            // Play the door opening sound effect
             SFXManager.instance.PlaySFX(1);
         }
     }
 
+    // Initiates the crossfade animation and loads the next level after a delay.
     public void NextLevelCrossfade()
     {
+        // Start the crossfade animation and load the next level
         StartCoroutine(loadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        // Fade out the background music
         StartCoroutine(FadeOutMusic());
     }
 
+    // Coroutine to load a level with a crossfade animation.
     IEnumerator loadLevel(int levelIndex)
     {
+        // Trigger the crossfade animation
         crossfadeAnimator.SetTrigger("Start");
 
+        // Wait for the specified transition time
         yield return new WaitForSeconds(transitionTime);
 
+        // Load the next scene
         SceneManager.LoadScene(levelIndex);
     }
 
+    // Coroutine to fade out the background music.
     IEnumerator FadeOutMusic()
     {
+        // If the audio source is not assigned, try to get it from the component
         if (audioSource == null)
         {
             audioSource = GetComponent<AudioSource>();
         }
 
+        // Store the initial volume of the audio source
         float startVolume = audioSource.volume;
+        // Gradually decrease the volume of the background music
         while (audioSource.volume > 0)
         {
             audioSource.volume -= startVolume * Time.deltaTime / musicFadeOutTime;
             yield return null;
         }
 
+        // Stop the music when the volume reaches zero
         audioSource.Stop();
     }
 }
